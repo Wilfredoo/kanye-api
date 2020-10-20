062b92b2726e83ff391a9165ace8961ca4fcc745
Wilfredo <casas.farach@gmail.com>
2020-10-20 15:16:44 +0200

    fetching data from kanya api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Option A: class component
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quote: "" };
+  }
+
+  async getQuote() {
+    let response = await fetch("https://api.kanye.rest?format=text");
+    let data = await response.text();
+    console.log("what is this? -->", data);
+    this.setState({ quote: data }, () => {
+      console.log("this state", this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() => this.getQuote()}>Get Quote from Kanye</button>
+        {this.state.quote && <p>{this.state.quote}</p>}
+      </div>
+    );
+  }
 }
+
+
+// Option B: functional component
+// const App = () => {
+//   const [quote, setQuote] = useState();
+//   async function getQuote() {
+//     let response = await fetch('https://api.kanye.rest?format=text');
+//     let data = await response.text();
+//     console.log("what is this? -->",data);
+//     setQuote(data)
+// }
+
+//   return (
+//     <div className="App">
+//       <button onClick={() => getQuote()}>Get Quote from Kanye</button>
+//       {quote && <p>{quote}</p>}
+//     </div>
+//   );
+// }
 
 export default App;
